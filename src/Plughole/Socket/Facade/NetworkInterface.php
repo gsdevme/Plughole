0ece7a4f6bba5465660fc22f2b643b0aee5e7063
Gavin Staniforth <gavinstaniforth@wems.co.uk>
2015-01-12 20:58:52 +0000

    derped forgot to fix the interface

--- src/Plughole/Socket/Facade/NetworkInterface.php
+++ src/Plughole/Socket/Facade/NetworkInterface.php
@@ -4,5 +4,5 @@ namespace Plughole\Socket\Facade;
 
 interface NetworkInterface
 {
-    public function fsocketopen($hostname, $port = -1, &$errno, &$error, $timeout = null);
+    public function fsocketopen($hostname, $port = -1, &$errno = null, &$error = null, $timeout = null);
 }
